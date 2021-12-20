460862a7d31577ab75a0dca6230ea6e3e5d822ea
Matti Paksula <matti.paksula@iki.fi>
2021-12-20 22:16:35 +0200

    fix reruns

--- userdata.sh
+++ userdata.sh
@@ -13,11 +13,11 @@ cd /root
   apt-get update && apt-get upgrade -y
   apt-get install -y git screen socat
 
-  if [ ! command -v docker ]; then
+  if ! command -v docker; then
     curl -fsSL https://get.docker.com | sh
   fi
 
-  if [ ! command -v docker-compose ]; then
+  if ! command -v docker-compose; then
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
   fi
@@ -27,6 +27,11 @@ cd /root
   fi
 
   cd self-hosted
+    git checkout master
+    set +e
+      git branch delete -D latest
+    set -e
+
     git fetch --all --tags
     git checkout "tags/${SENTRY_TAG}" -b latest
 
